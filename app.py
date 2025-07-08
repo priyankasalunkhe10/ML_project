42063db52fcd779a82b119a09d6d4d9e9c2e7d35
unknown <salunkhepriyanka6542@gmail.com>
2025-07-08 18:49:38 +0530

    this is my first commit

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-print('pune')
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('flight_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        user_input = {
+            'airline': request.form['airline'],
+            'source': request.form['source'],
+            'destination': request.form['destination'],
+            'date': request.form['journey_date'],
+            'stops': request.form['stops'],
+            'dep_time': request.form['dep_time'],
+            'arr_time': request.form['arr_time'],
+            'duration': request.form['duration']
+        }
+        price = model.predict()
+        
+        return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
